3cd1e935bced9dcc5c6b82efb9aa5a182fde4e6e
Muhammad Waqas Ilyas <qaskhan02@gmail.com>
2023-04-26 21:37:57 +0300

    tetsing

--- src/Controllers/DatabaseController.js
+++ src/Controllers/DatabaseController.js
@@ -1687,47 +1687,47 @@ class DatabaseController {
     await this.loadSchema().then(schema => schema.enforceClassExists('_Role'));
     await this.loadSchema().then(schema => schema.enforceClassExists('_Idempotency'));
 
-    await this.adapter.ensureUniqueness('_User', requiredUserFields, ['username']).catch(error => {
-      logger.warn('Unable to ensure uniqueness for usernames: ', error);
-      throw error;
-    });
+    // await this.adapter.ensureUniqueness('_User', requiredUserFields, ['username']).catch(error => {
+    //   logger.warn('Unable to ensure uniqueness for usernames: ', error);
+    //   throw error;
+    // });
 
-    await this.adapter
-      .ensureIndex('_User', requiredUserFields, ['username'], 'case_insensitive_username', true)
-      .catch(error => {
-        logger.warn('Unable to create case insensitive username index: ', error);
-        throw error;
-      });
-    await this.adapter
-      .ensureIndex('_User', requiredUserFields, ['username'], 'case_insensitive_username', true)
-      .catch(error => {
-        logger.warn('Unable to create case insensitive username index: ', error);
-        throw error;
-      });
+    // await this.adapter
+    //   .ensureIndex('_User', requiredUserFields, ['username'], 'case_insensitive_username', true)
+    //   .catch(error => {
+    //     logger.warn('Unable to create case insensitive username index: ', error);
+    //     throw error;
+    //   });
+    // await this.adapter
+    //   .ensureIndex('_User', requiredUserFields, ['username'], 'case_insensitive_username', true)
+    //   .catch(error => {
+    //     logger.warn('Unable to create case insensitive username index: ', error);
+    //     throw error;
+    //   });
 
-    await this.adapter.ensureUniqueness('_User', requiredUserFields, ['email']).catch(error => {
-      logger.warn('Unable to ensure uniqueness for user email addresses: ', error);
-      throw error;
-    });
+    // await this.adapter.ensureUniqueness('_User', requiredUserFields, ['email']).catch(error => {
+    //   logger.warn('Unable to ensure uniqueness for user email addresses: ', error);
+    //   throw error;
+    // });
 
-    await this.adapter
-      .ensureIndex('_User', requiredUserFields, ['email'], 'case_insensitive_email', true)
-      .catch(error => {
-        logger.warn('Unable to create case insensitive email index: ', error);
-        throw error;
-      });
+    // await this.adapter
+    //   .ensureIndex('_User', requiredUserFields, ['email'], 'case_insensitive_email', true)
+    //   .catch(error => {
+    //     logger.warn('Unable to create case insensitive email index: ', error);
+    //     throw error;
+    //   });
 
-    await this.adapter.ensureUniqueness('_Role', requiredRoleFields, ['name']).catch(error => {
-      logger.warn('Unable to ensure uniqueness for role name: ', error);
-      throw error;
-    });
+    // await this.adapter.ensureUniqueness('_Role', requiredRoleFields, ['name']).catch(error => {
+    //   logger.warn('Unable to ensure uniqueness for role name: ', error);
+    //   throw error;
+    // });
 
-    await this.adapter
-      .ensureUniqueness('_Idempotency', requiredIdempotencyFields, ['reqId'])
-      .catch(error => {
-        logger.warn('Unable to ensure uniqueness for idempotency request ID: ', error);
-        throw error;
-      });
+    // await this.adapter
+    //   .ensureUniqueness('_Idempotency', requiredIdempotencyFields, ['reqId'])
+    //   .catch(error => {
+    //     logger.warn('Unable to ensure uniqueness for idempotency request ID: ', error);
+    //     throw error;
+    //   });
 
     const isMongoAdapter = this.adapter instanceof MongoStorageAdapter;
     const isPostgresAdapter = this.adapter instanceof PostgresStorageAdapter;
